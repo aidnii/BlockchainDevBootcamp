2b049f2f3ccb713372be28575f936f2064e32739
cryptichic <syberchief@gmail.com>
2022-04-10 19:13:35 -0500

    added test, tracks the name

---
+++ blockchain-developer-bootcamp/test/Token.test.js
@@ -0,0 +1,24 @@
+const Token = artifacts.require('./Token')
+
+require('chai').use(require('chai-as-promised')).should()
+
+contract('Token', (accounts) => {
+    const name = 'my name'
+    const symbol = 'Symbol'
+    const decimals = 10
+    const totalSupply = 10
+    let token 
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
+    
+    describe('deployment', () => {
+        it('tracks the name', async () => {
+            const token = await Token.new()
+            const result = await token.name()
+            result.should.equal(name)
+        })
+
+    })
+})
