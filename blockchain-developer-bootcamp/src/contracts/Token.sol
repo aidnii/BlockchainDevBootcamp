f1529cb44cc8bc4949ddf32ca3d139c28cfd9b85
cryptichic <syberchief@gmail.com>
2022-04-11 19:59:08 -0500

    refactored code, added _transfer function

--- blockchain-developer-bootcamp/src/contracts/Token.sol
+++ blockchain-developer-bootcamp/src/contracts/Token.sol
@@ -21,12 +21,16 @@ contract Token {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(_to != address(0));
         require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
-        return true;
     }
 
     // Approve Tokens
@@ -38,4 +42,8 @@ contract Token {
     }
 
     // Transfer from 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+
+        return true;
+    }
 }
