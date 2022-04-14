42801bb7a3f93e168d36f3fefaa363e9b6fe7bb8
cryptichic <syberchief@gmail.com>
2022-04-14 11:08:01 -0500

    created test, should track the fee account

--- blockchain-developer-bootcamp/test/Exchange.test.js
+++ blockchain-developer-bootcamp/test/Exchange.test.js
@@ -1,23 +1,19 @@
 import { expect } from "chai";
 import { tokens, EVM_REVERT } from "./helpers";
 
-const Token = artifacts.require("./Token");
+const Exchange = artifacts.require("./Exchange");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Token", ([deployer, receiver, exchange]) => {
-  const name = "Web3 Talent Agency";
-  const symbol = "WTA";
-  const decimals = "18";
-  const totalSupply = tokens(1000000).toString();
-  let token;
+contract("Exchange", ([deployer, feeAccount]) => {
+  let exchange;
 
   beforeEach(async () => {
-    token = await Token.new();
+    exchange = await Exchange.new();
   });
 
   describe("deployment", () => {
-    it("", async () => {
+    it("should track the fee account", async () => {
       
     });
 
