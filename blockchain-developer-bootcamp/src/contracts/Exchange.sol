28dc9654ac38f3a90dd89aeac6acaf5d9c0e2497
cryptichic <syberchief@gmail.com>
2022-04-14 11:01:17 -0500

    created new file named Exchange.sol

---
+++ blockchain-developer-bootcamp/src/contracts/Exchange.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+// Creating an exchange that can Deposit & withdraw funds
+// Manage orders by making or canceling orders
+// handle trades - charge fees
+ 
+// To Do:
+// [ ] Set the account fee
+// [ ] Deposit ether
+// [ ] Withdraw ether
+// [ ] Deposit tokens
+// [ ] Withdraw tokens
+// [ ] Check Balances
+// [ ] Make order
+// [ ] Cancel order
+// [ ] Fill order
+// [ ] Charge fees
+
+contract Exchange {
+    
+}
+
