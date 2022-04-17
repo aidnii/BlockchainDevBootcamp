b293c5334cdd2d032bb1f6a317d509e290e2d613
cryptichic <syberchief@gmail.com>
2022-04-17 10:06:31 -0500

    added test, emits a withdraw event - passing test

--- blockchain-developer-bootcamp/test/Exchange.test.js
+++ blockchain-developer-bootcamp/test/Exchange.test.js
@@ -86,6 +86,16 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
         const balance = await exchange.tokens(ETHER_ADDRESS, user1);
         balance.toString().should.equal('0');
       });
+
+      it("emits a withdraw event", async () => {
+        const log = result.logs[0];
+        log.event.should.eq("Withdraw");
+        const event = log.args;
+        event.token.should.equal(ETHER_ADDRESS);
+        event.user.should.equal(user1);
+        event.amount.toString().should.equal(amount.toString());
+        event.balance.toString().should.equal('0');
+      });
     });
 
     describe("failure", async () => {
