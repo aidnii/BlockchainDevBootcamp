8067b8b97645ea9c48058a2e28a057d147b3a45b
cryptichic <syberchief@gmail.com>
2022-04-17 10:42:44 -0500

    added balanceof functionality to Exchange.sol contract

--- blockchain-developer-bootcamp/src/contracts/Exchange.sol
+++ blockchain-developer-bootcamp/src/contracts/Exchange.sol
@@ -69,6 +69,10 @@ contract Exchange {
         require(Token(_token).transfer(msg.sender, _amount));
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
+
+    function balanceOf(address _token, address _user) public view returns (uint256) {
+        return tokens[_token][_user];
+    }
 }
 
 
